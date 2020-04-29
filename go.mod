86f483de523ee875a9358b5696cce06857b0da59
Dmitriy <dmitrijsazonov@Dmitrijs-MacBook-Pro.local>
2020-04-29 11:45:15 +0300

    Add get to cli

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/soypita/go-shipping-cli
 
 go 1.14
 
-require github.com/soypita/go-shipping v0.0.0-20200428224854-69b2f35739fb
+require (
+	github.com/soypita/go-shipping v0.0.0-20200429083414-07aebc1a588b
+	google.golang.org/grpc v1.29.1
+)
--- main.go
+++ main.go
@@ -49,5 +49,13 @@ func main() {
 	if err != nil {
 		log.Fatalf("Could not greet: %v", err)
 	}
-	log.Printf("Created: %t", r.Created)
+	log.Printf("Created: %t : %v", r.Created, r.Consignment)
+
+	getAll, err := client.GetConsignments(context.Background(), &pb.GetRequest{})
+	if err != nil {
+		log.Fatalf("Could not list consignments: %v", err)
+	}
+	for _, v := range getAll.Consignments {
+		log.Println(v)
+	}
 }
